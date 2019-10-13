89118a10c0b2a9d458449859e8039b4c91d16f91
atif-sreezon <n10560238@qut.edu.au>
2019-10-13 14:47:25 +1000

    Add GUI

---
+++ GUI_Practice/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace GUI_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.panelSide = new System.Windows.Forms.Panel();
+            this.btnIndex = new System.Windows.Forms.Button();
+            this.btnDirectoryBr = new System.Windows.Forms.Button();
+            this.tbDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbFName = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbSearchBar = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbResults = new System.Windows.Forms.TextBox();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            this.panelSide.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(786, 32);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans Extrabold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(18, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minerva";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(713, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.DimGray;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(753, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(32, 32);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnClose.TabIndex = 1;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            this.btnClose.MouseLeave += new System.EventHandler(this.BtnClose_MouseLeave);
+            this.btnClose.MouseHover += new System.EventHandler(this.BtnClose_MouseHover);
+            // 
+            // panelSide
+            // 
+            this.panelSide.BackColor = System.Drawing.Color.LightGray;
+            this.panelSide.Controls.Add(this.circularProgressBar1);
+            this.panelSide.Controls.Add(this.btnIndex);
+            this.panelSide.Controls.Add(this.btnDirectoryBr);
+            this.panelSide.Controls.Add(this.tbDirectory);
+            this.panelSide.Controls.Add(this.btnBrowse);
+            this.panelSide.Controls.Add(this.tbFName);
+            this.panelSide.Controls.Add(this.panel3);
+            this.panelSide.Controls.Add(this.panel2);
+            this.panelSide.Controls.Add(this.label3);
+            this.panelSide.Controls.Add(this.label1);
+            this.panelSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSide.Location = new System.Drawing.Point(0, 32);
+            this.panelSide.Name = "panelSide";
+            this.panelSide.Size = new System.Drawing.Size(202, 414);
+            this.panelSide.TabIndex = 1;
+            this.panelSide.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelSide_Paint);
+            // 
+            // btnIndex
+            // 
+            this.btnIndex.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIndex.Location = new System.Drawing.Point(17, 299);
+            this.btnIndex.Name = "btnIndex";
+            this.btnIndex.Size = new System.Drawing.Size(93, 36);
+            this.btnIndex.TabIndex = 4;
+            this.btnIndex.Text = "Index";
+            this.btnIndex.UseVisualStyleBackColor = true;
+            this.btnIndex.Click += new System.EventHandler(this.BtnIndex_Click);
+            // 
+            // btnDirectoryBr
+            // 
+            this.btnDirectoryBr.BackColor = System.Drawing.Color.Silver;
+            this.btnDirectoryBr.FlatAppearance.BorderSize = 0;
+            this.btnDirectoryBr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDirectoryBr.Font = new System.Drawing.Font("Open Sans Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDirectoryBr.Location = new System.Drawing.Point(114, 263);
+            this.btnDirectoryBr.Name = "btnDirectoryBr";
+            this.btnDirectoryBr.Size = new System.Drawing.Size(75, 29);
+            this.btnDirectoryBr.TabIndex = 3;
+            this.btnDirectoryBr.Text = "Directory";
+            this.btnDirectoryBr.UseVisualStyleBackColor = false;
+            this.btnDirectoryBr.Click += new System.EventHandler(this.BtnDirectoryBr_Click);
+            // 
+            // tbDirectory
+            // 
+            this.tbDirectory.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDirectory.Location = new System.Drawing.Point(17, 232);
+            this.tbDirectory.Name = "tbDirectory";
+            this.tbDirectory.Size = new System.Drawing.Size(172, 25);
+            this.tbDirectory.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.Silver;
+            this.btnBrowse.FlatAppearance.BorderSize = 0;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("Open Sans Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(114, 102);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 31);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // tbFName
+            // 
+            this.tbFName.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFName.Location = new System.Drawing.Point(17, 71);
+            this.tbFName.Name = "tbFName";
+            this.tbFName.Size = new System.Drawing.Size(172, 25);
+            this.tbFName.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Silver;
+            this.panel3.Location = new System.Drawing.Point(17, 211);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(172, 2);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Location = new System.Drawing.Point(17, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(172, 2);
+            this.panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 52);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Select Lucene\r\nDirectory";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select JSON File";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.tbSearchBar);
+            this.groupBox1.Font = new System.Drawing.Font("Open Sans Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(221, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(553, 58);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(453, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // tbSearchBar
+            // 
+            this.tbSearchBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbSearchBar.Location = new System.Drawing.Point(7, 25);
+            this.tbSearchBar.Name = "tbSearchBar";
+            this.tbSearchBar.Size = new System.Drawing.Size(440, 25);
+            this.tbSearchBar.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbResults);
+            this.groupBox2.Font = new System.Drawing.Font("Open Sans Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(221, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(553, 300);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search Results";
+            // 
+            // tbResults
+            // 
+            this.tbResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResults.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbResults.Location = new System.Drawing.Point(7, 24);
+            this.tbResults.Multiline = true;
+            this.tbResults.Name = "tbResults";
+            this.tbResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbResults.Size = new System.Drawing.Size(540, 270);
+            this.tbResults.TabIndex = 0;
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(114, 339);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.circularProgressBar1.ProgressWidth = 25;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(70, 70);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = ".23";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "°C";
+            this.circularProgressBar1.TabIndex = 5;
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar1.Value = 68;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(786, 446);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panelSide);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "SearchEngine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.panelSide.ResumeLayout(false);
+            this.panelSide.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox btnClose;
+        private System.Windows.Forms.Panel panelSide;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDirectoryBr;
+        private System.Windows.Forms.TextBox tbDirectory;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbFName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbSearchBar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbResults;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIndex;
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+    }
+}
+
---
+++ GUI_Practice/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Lucene.Net.Search; // for IndexSearcher
+
+namespace GUI_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        LuceneApp myLuceneApp = new LuceneApp();
+
+        #region Button Functions
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnClose_MouseHover(object sender, EventArgs e)
+        {
+            this.btnClose.BackColor = Color.FromArgb(52,184,214);
+        }
+
+        private void BtnClose_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnClose.BackColor = Color.DimGray;
+        }
+
+        private void PanelSide_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "JSON|*.json";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tbFName.Text = ofd.SafeFileName;
+                ofd.Dispose();
+            }
+        }
+
+        private void BtnDirectoryBr_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbDirectory.Text = fbd.SelectedPath;
+                ResultsUpdate("Lucene Index Directory: " + tbDirectory.Text);
+                fbd.Dispose();
+            }
+        }
+
+        private void BtnIndex_Click(object sender, EventArgs e)
+        {
+            Form1 myForm = new Form1();
+            string json = File.ReadAllText(ofd.FileName);
+            string curPath = Directory.GetCurrentDirectory();
+
+            List<records> storeRecords = DeserializeJSON(json);
+
+            myLuceneApp.CreateIndex(tbDirectory.Text);
+
+            System.DateTime IndexStart = DateTime.Now;
+            for (int x = 0; x < storeRecords.Count; x++)
+            {
+                ResultsUpdate("Adding record no #"+ (x + 1));
+                circularProgressBar1.Value = ((x + 1) / storeRecords.Count) * 100;
+                circularProgressBar1.Update();
+                for (int y = 0; y < storeRecords[x].passages.Count; y++)
+                {
+                    //WriteLine("URL: {0}", storeRecords[x].passages[y].url.ToString());
+                    //WriteLine("Passage Text: {0}", storeRecords[x].passages[y].passage_text.ToString());
+                    myLuceneApp.IndexText(storeRecords[x].passages[y].url, storeRecords[x].passages[y].passage_text);
+                    //LuceneApp.IndexText(storeRecords[x].passages[y].passage_text);
+                }
+            }
+            System.DateTime IndexEnd = DateTime.Now;
+            TimeSpan span = (IndexEnd - IndexStart);
+            ResultsUpdate("All documents added");
+            ResultsUpdate("Total Index time: " + span.Minutes + "minute(s) " +
+                span.Seconds + "seconds.");
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            ResultsUpdate("Searching for " + tbSearchBar);
+
+            myLuceneApp.CleanUpIndexer();
+            myLuceneApp.CreateSearcher();
+            myLuceneApp.CreateParser();
+            //myLuceneApp.SearchIndex(DisplayTopDoc(tbSearchBar.Text));
+            DisplayTopDoc(myLuceneApp.SearchIndex(tbSearchBar.Text));
+        }
+        #endregion
+
+        const string TEXT_URL = "URL"; //Text Fields. For the column URL column.
+        const string TEXT_PASSAGE = "Passage Text"; //Text fields. For the Passage Text column.
+        private void DisplayTopDoc(Lucene.Net.Search.TopDocs results)
+        {
+            int rank = 0;
+            var searcher = myLuceneApp.CreateSearcher();
+            Lucene.Net.Documents.Document doc = null;
+
+            foreach (ScoreDoc scoreDoc in results.ScoreDocs)
+            {
+                rank++;
+                // retrieve the document from the 'ScoreDoc' object
+                doc = searcher.Doc(scoreDoc.Doc);
+                string field_URL = doc.Get(TEXT_URL).ToString();
+                string field_Text = doc.Get(TEXT_PASSAGE).ToString();
+                ResultsUpdate("Rank #" + rank);
+                ResultsUpdate("\n");
+                ResultsUpdate("Rank #" + rank);
+                ResultsUpdate("URL: " + field_URL);
+                ResultsUpdate("Passage Text: ");
+                ResultsUpdate(field_Text);
+                ResultsUpdate("\n");
+            }
+        }
+        #region json functions
+
+        public static List<records> DeserializeJSON(string jsonText)
+        {
+            Form1 myForm = new Form1();
+            try
+            {
+                var jsonList = JsonConvert.DeserializeObject<List<records>>(jsonText);
+                return jsonList;
+            }
+            catch (Exception ex)
+            {
+                myForm.ResultsUpdate("ERROR: " + ex.Message.ToString());
+                return null;
+            }
+
+        }
+        #endregion
+
+        #region Display
+        public void ResultsUpdate(string stringText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(stringText + Environment.NewLine);
+                tbResults.Text = tbResults.Text + stringText + Environment.NewLine;
+                tbResults.SelectionStart = tbResults.TextLength;
+                tbResults.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message.ToString() + Environment.NewLine);
+            }
+        }
+
+
+
+        #endregion
+
+        #region Lucene Codes
+
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            circularProgressBar1.Value = 0;
+            circularProgressBar1.Minimum = 0;
+            circularProgressBar1.Maximum = 100;
+        }
+    }
+}
